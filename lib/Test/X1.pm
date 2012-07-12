6a42a575e42fcdd865ba0164559cc64c5e1008f7
wakabatan <wakabatan@localhost.localdomain>
2012-07-12 11:46:36 +0900

    synrax

--- lib/Test/X1.pm
+++ lib/Test/X1.pm
@@ -190,7 +190,7 @@ sub run_tests {
     $schedule_test = sub {
         if (@test) {
             my $test = shift @test;
-            my $test_cv = AE::cv;
+            my $test_cv = AE::cv();
 
             my $test_name;
             my $context = $context_class->new(
